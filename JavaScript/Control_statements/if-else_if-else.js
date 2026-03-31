bdc5fc8ccd5a24acc217cd319e7f55b4dc4cdd84
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 23:23:22 -0600

    Adding if-else if else example

---
+++ JavaScript/Control_statements/if-else_if-else.js
@@ -0,0 +1,16 @@
+/* 
+This is program explains an example of if-else if-else statement
+*/
+
+let myNumber = 0;
+
+// Checking if the value is positive
+if(myNumber > 0){
+    console.log(`Positive Value -> ${myNumber}`)
+}
+else if (myNumber < 0){
+    console.log(`Negative Value -> ${myNumber}`)
+}
+else{
+    console.log(`Zero value -> ${myNumber} `)
+}
