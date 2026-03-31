3bf347b2a35582f1f758e2e5aa0611e5850f5056
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 20:06:33 -0600

    Adding constants.js

---
+++ JavaScript/Variables/constants.js
@@ -0,0 +1,15 @@
+/* 
+This program explains the use of constants
+
+Author: Lalo Téllez
+*/
+
+const myConstant = 10;
+// myConstant = 20; // We cannot change the value of a constants
+console.log(myConstant);
+
+console.log(Math.PI);
+
+// Sec per minute
+const SECONDS_PER_MINUTE = 60;
+console.log(SECONDS_PER_MINUTE);
