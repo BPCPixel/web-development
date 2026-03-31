92d004f40f4a7a6fefffe6037cd5f74064e4d599
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 22:02:34 -0600

    Adding more examples to comparation_operators.js

--- JavaScript/Operators/comparation_operators.js
+++ JavaScript/Operators/comparation_operators.js
@@ -22,6 +22,22 @@ console.log("a === b ->" , a === b);
 // String interpolation
 console.log(`${a} === ${b} -> ${a === b}`);
 
+// Different operators
+// Compares value and converts type of if necessary
+console.log(`${a} != ${b} -> ${a != b}`);
 
+// Exact and different operators
+// Compares value and converts type of if necessary
+console.log(`${a} !== ${b} -> ${a !== b}`);
 
+// Less than
+console.log(`${a} < ${b} -> ${a < b}`);
 
+// Less or equal than
+console.log(`${a} <= ${b} -> ${a <= b}`);
+
+// Greater than
+console.log(`${a} > ${b} -> ${a > b}`);
+
+// Greater or iqual than
+console.log(`${a} >= ${b} -> ${a >= b}`);
