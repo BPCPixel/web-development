648af3bfaf6a4be62442f4e03d65bf1eda121825
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 20:16:36 -0600

    Adding arithmetic_operators.js

---
+++ JavaScript/Operators/arithmetic_operators.js
@@ -0,0 +1,32 @@
+/* 
+This program explains the use arithmetic operators
+
+Author: Lalo Téllez
+*/
+
+let a, b, c, d, e, f;
+
+// Addition
+a = 3 + 4;
+console.log(a);
+
+// Difference
+b = 6 - 2;
+console.log(b);
+
+// Multiplication
+c = a * 2;
+console.log(c);
+
+// Division
+d = b / 2.5;
+console.log(d);
+console.log(typeof d);
+
+// Module
+f = 9 % 2;
+console.log(f);
+
+// Pow
+e = 2 ** 3;
+console.log(e);
