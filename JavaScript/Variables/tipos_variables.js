30e3a10c9eeeb264d427c65eeffcdf86d6d6becb
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 19:03:55 -0600

    Setting programs of JS to English and adding types of variables

---
+++ JavaScript/Variables/tipos_variables.js
@@ -0,0 +1,13 @@
+/*
+This program explains types of variables
+
+Author: Lalo Téllez
+*/
+
+// Unused variable
+var num = 15;
+console.log(num)
+
+// Recommended used of variables
+let num2 = 20;
+console.log(num2)
