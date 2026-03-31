92921982109fb000c7569965763d8a242ef401a1
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 23:27:49 -0600

    Adding ternay_operator.js example

---
+++ JavaScript/Control_statements/ternary_operator.js
@@ -0,0 +1,19 @@
+/* 
+This is program explains an example ternary operators
+*/
+
+let myNumber = 10;
+
+/* Checking if the value is positive
+if(myNumber > 0){
+    console.log(`Positive Value -> ${myNumber}`)
+}
+else if (myNumber < 0){
+    console.log(`Negative Value -> ${myNumber}`)
+}
+else{
+    console.log(`Zero value -> ${myNumber} `)
+}
+*/
+
+(myNumber > 0) ? console.log(`Positive`) : console.log(`Negative`)
