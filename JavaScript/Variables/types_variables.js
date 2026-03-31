339b667d6d3bc3430b005159fcf896e6885f92df
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 19:11:30 -0600

    Editing types of variables, adding all the types with examples

--- JavaScript/Variables/types_variables.js
+++ JavaScript/Variables/types_variables.js
@@ -6,8 +6,27 @@ Author: Lalo Téllez
 
 // Unused variable
 var num = 15;
-console.log(num)
+console.log(num);
 
 // Recommended used of variables
 let num2 = 20;
-console.log(num2)
+console.log(num2);
+
+// Types of variables
+let myInteger = 10;
+console.log(myInteger);
+
+let myFloat = 4.6;
+console.log(myFloat);
+
+let myString = "Hello";
+console.log(myString);
+
+let myBoolean = true;
+console.log(myBoolean);
+
+let myNull = null;
+console.log(myNull);
+
+let myUndefined = undefined;
+console.log(myUndefined);
