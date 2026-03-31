cb9ac096105bb82e91e4a242abe9add8e5abad48
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 20:31:30 -0600

    Adding assignment_operators.js

---
+++ JavaScript/Operators/assignment_operators.js
@@ -0,0 +1,38 @@
+/* 
+This program explains the use of assignment operators
+
+Author: Lalo Téllez
+*/
+
+let myNumber = 10;
+console.log(myNumber);
+
+myNumber = 20;
+console.log(myNumber);
+
+// Operator +=
+myNumber += 5;
+console.log(myNumber);
+
+// Operator -=
+myNumber -= 3;
+console.log(myNumber);
+
+// Operator *=
+myNumber *= 2;
+console.log(myNumber);
+
+// Operator /=
+myNumber /= 4;
+console.log(myNumber);Ñ
+
+// Operator %=
+myNumber %= 2;
+console.log(myNumber);
+
+// Pow **=
+myNumber = 8;
+myNumber **= 2;
+console.log(myNumber);
+
+
