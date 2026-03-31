cbbdb9e18ec8191bf440951c489cdca9606d9eb4
Lalo Tellez <bpcpixel@gmail.com>
2026-03-30 23:09:59 -0600

    Adding if_else program to JS

---
+++ JavaScript/Control_statements/if_else.js
@@ -0,0 +1,12 @@
+/* 
+This is program explains an example of if else statement
+*/
+
+let myNumber = -10;
+
+// Checking if the value is positive
+if(myNumber > 0){
+    console.log(`Positive Value -> ${myNumber}`)
+}else{
+    console.log(`Negative Value -> ${myNumber}`)
+}
