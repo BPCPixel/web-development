29db95f30f01f138e7439f2e9543796520ef21fd
Lalo Tellez <bpcpixel@gmail.com>
2026-04-01 12:17:47 -0600

    Adding concatenation.js from Curso 2

---
+++ JavaScript/Curso_2/js/concatenation.js
@@ -0,0 +1,12 @@
+// Concatenaciones
+const nombre = "Eduardo"
+const email = "correo@correo.com"
+
+console.log(nombre);
+console.log(email);
+
+// Concatenaciones
+console.log("Nombre cliente: ", nombre, "Email: ", email);
+
+// Template Strings
+console.log(`Nombre cliente: ${nombre} \nEmail: ${email}`);
