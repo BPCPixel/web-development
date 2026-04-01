bc3eb150de97a0e96eaab1da4af9afc622a37656
Lalo Tellez <bpcpixel@gmail.com>
2026-04-01 10:19:47 -0600

    Adding strings1.js and obj_math.js

---
+++ JavaScript/Curso_2/js/obj_math.js
@@ -0,0 +1,32 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5); // Redondea de .5 hacia arriba
+console.log(resultado);
+
+resultado = Math.ceil(2.1);  // Siempre redondea hacia arriba
+console.log(resultado);
+
+resultado = Math.floor(2.9);  // Siempre redondea hacia abajo
+console.log(resultado);
+
+resultado = Math.sqrt(144);  // Raiz cuadrada
+console.log(resultado);
+
+resultado = Math.abs(-2);  // Un número negativo lo convierte a positivo
+console.log(resultado);
+
+resultado = Math.min(2,5,78,1, 4, 6);  // Aquí nos indica cual es el número más pequeño de un listado
+console.log(resultado);
+
+resultado = Math.max(2,5,78,1, 4, 6);  // Aquí nos indica cual es el número más grande de un listado
+console.log(resultado);
+
+resultado = Math.random();  // Nos muestra un número aleatorio menos de 1
+console.log(resultado);
+
+resultado = Math.floor( Math.random() * 100 );  // Nos muestra un número aleatorio hasta el 100
+console.log(resultado);
+
---
+++ JavaScript/Curso_2/js/strings1.js
@@ -0,0 +1,22 @@
+
+// String o cadena de texto
+const producto = "Monitor de 20 Pulgadas";
+console.log(producto);
+
+const producto2 = "Monitor de 30 Pulgadas";
+console.log(producto2);
+
+// Observar el tipo de dato
+const producto3 = "Monitor de 50 Pulgadas";
+console.log(typeof producto3);
+
+// Métodos
+console.log(producto.length); // Método para saber la longitud de la cadena producto
+
+// IndexOf para decirme en que posición se encuentra una cadena que esté buscando
+const curso = "Curso de JavaScript";
+console.log(curso.indexOf('JavaScript'))
+
+// Includes (retorna true o false) solo avisa si la encontró o noConflict
+console.log(curso.includes('JavaScript'))
+
