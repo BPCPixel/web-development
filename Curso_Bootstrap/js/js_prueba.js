8cb79e20c04424b4600781e24203fa8e1048970e
Lalo Tellez <bpcpixel@gmail.com>
2026-04-02 19:00:00 -0600

    saving stages of git

---
+++ Curso_Bootstrap/js/js_prueba.js
@@ -0,0 +1,11 @@
+    $("#miBotonVerde").click(function(){ 
+                alert("Presionaste el Botón Verde"); 
+    });
+
+    $("#miBotonRojo").click(function(){
+        alert("Presionaste el Botón Rojo");
+    });
+
+    $("#miBotonAzul").click(function(){ 
+        alert("Presionaste el Botón Azul");
+    });
