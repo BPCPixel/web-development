b8f8d3401152b21a3e237d80dd7007ab3fad6ed0
Lalo Tellez <bpcpixel@gmail.com>
2026-04-03 19:00:00 -0600

    Adding arrays.js

---
+++ Curso_JS_Universidad/Control_statements/Arrays/arrays.js
@@ -0,0 +1,24 @@
+/*
+Arrays in Javascript
+
+Author: Lalo Téllez
+*/
+
+// Declaring array
+let arrayNumber = [];
+
+// Modifying values
+arrayNumber[0] = 13;
+arrayNumber[1] = 12;
+arrayNumber[2] = 11;
+arrayNumber[3] = 10;
+arrayNumber[4] = 'hola';
+
+// Printing array values
+console.log(arrayNumber);
+
+console.log(`Element 1 - [0]: ${arrayNumber[0]}`);
+console.log(`Element 2 - [1]: ${arrayNumber[1]}`);
+console.log(`Element 3 - [2]: ${arrayNumber[2]}`);
+console.log(`Element 4 - [3]: ${arrayNumber[3]}`);
+console.log(`Element 5 - [4]: ${arrayNumber[4]}`);
