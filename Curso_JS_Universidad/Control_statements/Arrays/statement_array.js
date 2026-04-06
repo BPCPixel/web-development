7ad0d1cb73388f656c9f980bdc35a0e47726c286
Lalo Tellez <bpcpixel@gmail.com>
2026-04-05 19:00:00 -0600

    Adding statement_array.js

---
+++ Curso_JS_Universidad/Control_statements/Arrays/statement_array.js
@@ -0,0 +1,16 @@
+/*
+Arrays in Javascript 
+
+Author: Lalo Téllez
+*/
+
+// Declaring array
+let arrayNumber = [13, 21, 0, 0, 62];
+
+// Printing the length of the array
+console.log(arrayNumber.length);
+
+// Reading values
+for(let i = 0; i < arrayNumber.length; i++){
+    console.log(arrayNumber[i]);
+}
