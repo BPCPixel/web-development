04fe540550cb4c91da9f7c5d3a0eaca9106e24a5
Lalo Tellez <bpcpixel@gmail.com>
2026-04-06 23:56:38 -0600

    Adding function_syntax.js

---
+++ Curso_JS_Universidad/Functions/function_syntax.js
@@ -0,0 +1,17 @@
+/*
+This program shows how the functions work
+
+Author: Lalo Téllez
+*/
+
+// Procedimiento en JS
+// 1. Definir Procedimiento
+
+function saludar(mensaje){
+    // Cuerpo de la función
+    console.log(`Mensaje: ${mensaje}`);
+}
+
+// 2. Llamar el Procedimiento
+saludar('Hola');
+
