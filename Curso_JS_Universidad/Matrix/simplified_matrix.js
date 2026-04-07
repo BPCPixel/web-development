c45f08b5cb3b44ded8c8ea4dcea22380cb680cbe
Lalo Tellez <bpcpixel@gmail.com>
2026-04-06 23:13:14 -0600

    Adding a different form of declaring a matrix in simplified_matrix.js

---
+++ Curso_JS_Universidad/Matrix/simplified_matrix.js
@@ -0,0 +1,13 @@
+/*
+This program shows a simplified matrix
+
+Author: Lalo Téllez
+*/
+
+// Declaring matrix
+let matrix = [[100, 200, 300],[400, 500, 600]];
+
+
+// Reading values
+console.log(`Element[0][1] = ${matrix[0][1]}`);
+console.log(`Element[0][1] = ${matrix[1][2]}`);
