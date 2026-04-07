b859704eed1b9e1939094e6d42d93b96ba04da5c
Lalo Tellez <bpcpixel@gmail.com>
2026-04-06 23:10:09 -0600

    Adding matrix.js

---
+++ Curso_JS_Universidad/Matrix/matrix.js
@@ -0,0 +1,23 @@
+/*
+This program shows how matrix works
+
+Author: Lalo Téllez
+*/
+
+// Declaring matrix
+let matrix = [[],[]];
+
+// Modifying values
+// ROW 0
+matrix[0][0] = 100;
+matrix[0][1] = 200;
+matrix[0][2] = 300;
+
+// ROW 1
+matrix[1][0] = 400;
+matrix[1][1] = 500;
+matrix[1][2] = 600;
+
+// Reading values
+console.log(`Element[0][1] = ${matrix[0][1]}`);
+console.log(`Element[0][1] = ${matrix[1][2]}`);
