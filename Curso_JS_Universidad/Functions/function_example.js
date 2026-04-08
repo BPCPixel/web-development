ec82d208c80c9414fd5492d7838a2f1c8643c156
Lalo Tellez <bpcpixel@gmail.com>
2026-04-08 12:45:26 -0600

    Adding an example of function

---
+++ Curso_JS_Universidad/Functions/function_example.js
@@ -0,0 +1,15 @@
+/*
+This program shows an example of a function
+
+Author: Lalo Téllez
+*/
+
+function suma(a,b){
+    let resultadoSuma = a + b;
+    return resultadoSuma;
+}
+
+let argA = 4, argB = 16;
+let resultado = suma(argA, argB);
+console.log(`El resultado de sumar ${argA} + ${argB} = ${resultado}`);
+
